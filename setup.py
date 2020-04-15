10748392b53b75ee2ed8754e6c6b3a13c4b5da39
victusfate <messel@gmail.com>
2020-04-15 15:17:53 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
   name='rsyslog_cee',
-  version='0.1.0',
+  version='0.1.1',
   description='python support for an rsyslog cee logger',
   url='https://github.com/victusfate/rsyslog_cee',
   author='victusfate',
