12b0190bbdbad86c6356a9d308500cf025d8f313
victusfate <messel@gmail.com>
2020-04-15 15:33:18 -0400

    keep 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
   name='rsyslog_cee',
-  version='0.1.1',
+  version='0.1.0',
   description='python support for an rsyslog cee logger',
   url='https://github.com/victusfate/rsyslog_cee',
   author='victusfate',
